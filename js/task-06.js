f741ef98bf102bf2936d7594d50517cbdd6802b4
Vadimde88 <Vadimde88@gmail.com>
2021-04-24 11:18:30 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,9 @@
 const inputRef = document.querySelector('#validation-input');
 
 inputRef.addEventListener('blur', (event) => {
-    if (inputRef.value.length > inputRef.dataset.length) {
-        return inputRef.id = 'validation-input_invalid';
+    if (inputRef.value.length == inputRef.dataset.length) {
+        return inputRef.id = 'validation-input_valid';
     } else { 
-    return inputRef.id = 'validation-input_valid';
+    return inputRef.id = 'validation-input_invalid';
 }
 });
